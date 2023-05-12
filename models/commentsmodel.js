959b9d8e4633452b4c5c9044fa226007afe28041
ThinakaranAntony <thinakaranxponential.digital>
2023-05-12 18:50:56 +0530

    changed

---
+++ models/commentsmodel.js
@@ -0,0 +1,18 @@
+const { Model } = require('objection');
+const knex = require('../config/dbconfig');
+Model.knex(knex);
+
+class Comments extends Model {
+    static get tableName() {
+        return 'comments';
+    }
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            properties: {
+                id: { type: 'integer' },
+            }
+        }
+    }
+}
+module.exports = Comments;
